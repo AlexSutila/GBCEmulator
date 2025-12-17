e2870af57a2d1f0d9df9c301a46948a94cff11e1
AlexSutila <alexsutila2020@gmail.com>
2025-12-16 17:02:24 -0700

    Sub and basic bitops instructions

--- includes/cpu/instr/alu.hpp
+++ includes/cpu/instr/alu.hpp
@@ -42,7 +42,6 @@ public:
       : Instruction(reg_file_ptr, bus_ptr) {}
   std::tuple<std::size_t, std::size_t> step() override {
     const byte_t a = read_reg<Register8Bit::REG_A>();
-    const byte_t imm = bus->read_byte(reg_file->reg_pc++);
     const byte_t result = a + imm;
 
     // Update flags
@@ -57,6 +56,10 @@ public:
     write_reg<Register8Bit::REG_A>(result);
     return {8, 8};
   }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
 };
 
 /*
@@ -99,8 +102,8 @@ public:
         reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK) ? 1 : 0;
 
     // Compute sum including carry flag
-    const addr_t sum = static_cast<uint16_t>(a) + static_cast<uint16_t>(x) +
-                       static_cast<uint16_t>(carry);
+    const addr_t sum = static_cast<addr_t>(a) + static_cast<addr_t>(x) +
+                       static_cast<addr_t>(carry);
     const byte_t result = static_cast<byte_t>(sum);
 
     // Update flags
@@ -122,13 +125,12 @@ public:
       : Instruction(reg_file_ptr, bus_ptr) {}
   std::tuple<std::size_t, std::size_t> step() override {
     const byte_t a = read_reg<Register8Bit::REG_A>();
-    const byte_t imm = bus->read_byte(reg_file->reg_pc++);
     const byte_t carry =
         reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK) ? 1 : 0;
 
     // Compute sum including carry flag
-    const addr_t sum = static_cast<uint16_t>(a) + static_cast<uint16_t>(imm) +
-                       static_cast<uint16_t>(carry);
+    const addr_t sum = static_cast<addr_t>(a) + static_cast<addr_t>(imm) +
+                       static_cast<addr_t>(carry);
     const byte_t result = static_cast<byte_t>(sum);
 
     // Update flags
@@ -139,6 +141,10 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, sum > 0xFF);
     return {8, 8};
   }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
 };
 
 /*
@@ -155,8 +161,8 @@ public:
         reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK) ? 1 : 0;
 
     // Compute sum including carry flag
-    const addr_t sum = static_cast<uint16_t>(a) + static_cast<uint16_t>(n) +
-                       static_cast<uint16_t>(carry);
+    const addr_t sum = static_cast<addr_t>(a) + static_cast<addr_t>(n) +
+                       static_cast<addr_t>(carry);
     const byte_t result = static_cast<byte_t>(sum);
 
     // Update flags
@@ -165,6 +171,471 @@ public:
     reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, sum > 0xFF);
+    return {8, 8};
+  }
+};
+
+/*
+ * Subtract value in 8-bit register X from A
+ */
+template <Register8Bit src> class SUB_A_X : public Instruction {
+public:
+  SUB_A_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t x = read_reg<src>();
+    const byte_t result = a - x;
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (x & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < x);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {4, 4};
+  }
+};
+
+/*
+ * Subtract immediate 8-bit value from A
+ */
+class SUB_A_imm8 : public Instruction {
+public:
+  SUB_A_imm8(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a - imm;
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (imm & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < imm);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
+};
+
+/*
+ * Subtract 8-bit value pointed to by HL to A
+ */
+class SUB_A_HL : public Instruction {
+public:
+  SUB_A_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t n = bus->read_byte(read_reg<Register16Bit::REG_HL>());
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a - n;
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (n & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < n);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+};
+
+/*
+ * Subtract value in 8-bit register X from A with carry
+ */
+template <Register8Bit src> class SBC_A_X : public Instruction {
+public:
+  SBC_A_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t x = read_reg<src>();
+    const byte_t carry =
+        reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK) ? 1 : 0;
+
+    // Compute difference including carry flag
+    const addr_t diff = static_cast<addr_t>(a) - static_cast<addr_t>(x) -
+                        static_cast<addr_t>(carry);
+    const byte_t result = static_cast<byte_t>(diff);
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (x & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < x);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {4, 4};
+  }
+};
+
+/*
+ * Subtract 8-bit immediate value from A with carry
+ */
+class SBC_A_imm8 : public Instruction {
+public:
+  SBC_A_imm8(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t carry =
+        reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK) ? 1 : 0;
+
+    // Compute difference including carry flag
+    const addr_t diff = static_cast<addr_t>(a) - static_cast<addr_t>(imm) -
+                        static_cast<addr_t>(carry);
+    const byte_t result = static_cast<byte_t>(diff);
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (imm & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < imm);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
+};
+
+/*
+ * Subtract 8-bit immediate value from A with carry
+ */
+class SBC_A_HL : public Instruction {
+public:
+  SBC_A_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t n = bus->read_byte(read_reg<Register16Bit::REG_HL>());
+    const byte_t carry =
+        reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK) ? 1 : 0;
+
+    // Compute difference including carry flag
+    const addr_t diff = static_cast<addr_t>(a) - static_cast<addr_t>(n) -
+                        static_cast<addr_t>(carry);
+    const byte_t result = static_cast<byte_t>(diff);
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (n & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < n);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+};
+
+/*
+ * Bitwise AND value from 8-bit register X with A
+ */
+template <Register8Bit src> class AND_A_X : public Instruction {
+public:
+  AND_A_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t x = read_reg<src>();
+    const byte_t result = a & x;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {4, 4};
+  }
+};
+
+/*
+ * Bitwise AND immediate 8-bit value X with A
+ */
+class AND_A_imm8 : public Instruction {
+public:
+  AND_A_imm8(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a & imm;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
+};
+
+/*
+ * Bitwise AND 8-bit value pointed to by HL with A
+ */
+class AND_A_HL : public Instruction {
+public:
+  AND_A_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t n = bus->read_byte(read_reg<Register16Bit::REG_HL>());
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a & n;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+};
+
+/*
+ * Bitwise XOR value from 8-bit register X with A
+ */
+template <Register8Bit src> class XOR_A_X : public Instruction {
+public:
+  XOR_A_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t x = read_reg<src>();
+    const byte_t result = a ^ x;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {4, 4};
+  }
+};
+
+/*
+ * Bitwise XOR immediate 8-bit value X with A
+ */
+class XOR_A_imm8 : public Instruction {
+public:
+  XOR_A_imm8(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a ^ imm;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
+};
+
+/*
+ * Bitwise XOR 8-bit value pointed to by HL with A
+ */
+class XOR_A_HL : public Instruction {
+public:
+  XOR_A_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t n = bus->read_byte(read_reg<Register16Bit::REG_HL>());
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a ^ n;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+};
+
+/*
+ * Bitwise OR value from 8-bit register X with A
+ */
+template <Register8Bit src> class OR_A_X : public Instruction {
+public:
+  OR_A_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t x = read_reg<src>();
+    const byte_t result = a | x;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {4, 4};
+  }
+};
+
+/*
+ * Bitwise OR immediate 8-bit value X with A
+ */
+class OR_A_imm8 : public Instruction {
+public:
+  OR_A_imm8(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a | imm;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
+};
+
+/*
+ * Bitwise OR 8-bit value pointed to by HL with A
+ */
+class OR_A_HL : public Instruction {
+public:
+  OR_A_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t n = bus->read_byte(read_reg<Register16Bit::REG_HL>());
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t result = a | n;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<Register8Bit::REG_A>(result);
+    return {8, 8};
+  }
+};
+
+/*
+ * Compare 8-bit register X with A. This is basically a subtract operation,
+ * but it throws away the result and only updates the flags.
+ */
+template <Register8Bit src> class CP_A_X : public Instruction {
+  CP_A_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+    const byte_t x = read_reg<src>();
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (x & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, a == x);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < x);
+    return {4, 4};
+  }
+};
+
+/*
+ * Compare immediate 8-bit value with A. This is basically a subtract operation,
+ * but it throws away the result and only updates the flags.
+ */
+class CP_A_imm8 : public Instruction {
+  CP_A_imm8(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (imm & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, a == imm);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < imm);
+    return {8, 8};
+  }
+  void parse() override { imm = bus->read_byte(reg_file->reg_pc++); }
+
+private:
+  byte_t imm;
+};
+
+/*
+ * Compare 8-bit value pointed to by HL with A. This is basically a subtract
+ * operation, but it throws away the result and only updates the flags.
+ */
+class CP_A_HL : public Instruction {
+  CP_A_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t n = bus->read_byte(read_reg<Register16Bit::REG_HL>());
+    const byte_t a = read_reg<Register8Bit::REG_A>();
+
+    // Update flags
+    const bool half_carry = (a & 0x0F) < (n & 0x0F);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, a == n);
+    reg_file->reg_af.set_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_H_MASK, half_carry);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, a < n);
     return {8, 8};
   }
 };
