66a17a0456d79acce3b3d0a74d4a8530773cf935
AlexSutila <alexsutila2020@gmail.com>
2025-12-17 19:06:59 -0700

    Formatting

--- includes/cart/cart_display.hpp
+++ includes/cart/cart_display.hpp
@@ -15,18 +15,16 @@ std::string sgb_flag_desc(byte_t sgb_flag);
 
 // Licensee: choose "old" unless old == 0x33, then use "new"
 std::string old_licensee_name(byte_t old_code);
-std::string new_licensee_name(const std::string& two_chars);
+std::string new_licensee_name(const std::string &two_chars);
 
 // Pretty sizes (based on header tags)
 std::string rom_size_pretty(byte_t rom_size_code);
 std::string ram_size_pretty(byte_t ram_size_code);
 
 // Render a full report
-std::string describe_cart(const cart& c);
+std::string describe_cart(const cart &c);
 
 // Print to stream
-void print_cart(std::ostream& os, const cart& c);
+void print_cart(std::ostream &os, const cart &c);
 
-
-
-#endif //GBC_CART_DISPLAY_HPP
+#endif // GBC_CART_DISPLAY_HPP
