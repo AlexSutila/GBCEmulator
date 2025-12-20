3ac3d5f693bc4f7d52cd9e9c9afe880a5f1d652f
AlexSutila <alexsutila2020@gmail.com>
2025-12-19 21:44:12 -0700

    Implement most CB prefix instructions

--- includes/cpu/instr/bitops.hpp
+++ includes/cpu/instr/bitops.hpp
@@ -121,7 +121,8 @@ public:
 };
 
 /*
- * The CB-prefix ISA extension
+ * The CB-prefix ISA extension. All instructions below this one lie under this
+ * instruction set architecture extention.
  */
 class CB_PREFIX : public Instruction {
 public:
@@ -136,6 +137,354 @@ private:
   /* This is identical in behavior to the lookup table in lr35902.hpp */
   void init_cb_prefix(lookup_table_t &lookup);
   lookup_table_t lookup{};
+};
+
+template <Register8Bit dst> class RLC_X : public Instruction {
+public:
+  RLC_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+
+    const bool carry = (x & 0x80) != 0x00;
+    byte_t result = (x << 1) | (carry ? 0x01 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class RLC_HL : public Instruction {
+public:
+  RLC_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+    const bool carry = (n & 0x80) != 0x00;
+    byte_t result = (n << 1) | (carry ? 0x01 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class RL_X : public Instruction {
+public:
+  RL_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const bool old_c = reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK);
+    const bool new_c = (x & 0x80) != 0x00;
+    const byte_t result = (x << 1) | (old_c ? 0x01 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, new_c);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class RL_HL : public Instruction {
+public:
+  RL_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t hl = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(hl);
+    const bool old_c = reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK);
+    const bool new_c = (n & 0x80) != 0x00;
+    const byte_t result = (n << 1) | (old_c ? 0x01 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, new_c);
+
+    // Write back
+    bus->write_byte(hl, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class RRC_X : public Instruction {
+public:
+  RRC_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const bool carry = (x & 0x01) != 0x00;
+    const byte_t result = (x >> 1) | (carry ? 0x80 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class RRC_HL : public Instruction {
+public:
+  RRC_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+
+    const bool carry = (n & 0x01) != 0x00;
+    const byte_t result = (n >> 1) | (carry ? 0x80 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class RR_X : public Instruction {
+public:
+  RR_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const bool old_c = reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK);
+    const bool new_c = (x & 0x01) != 0x00;
+    const byte_t result = (x >> 1) | (old_c ? 0x80 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, new_c);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class RR_HL : public Instruction {
+public:
+  RR_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+    const bool old_c = reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK);
+    const bool new_c = (n & 0x01) != 0x00;
+    const byte_t result = (n >> 1) | (old_c ? 0x80 : 0x00);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, new_c);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class SLA_X : public Instruction {
+public:
+  SLA_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const bool carry = (x & 0x80) != 0x00;
+    const byte_t result = x << 1;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class SLA_HL : public Instruction {
+public:
+  SLA_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+    const bool carry = (n & 0x80) != 0x00;
+    const byte_t result = n << 1;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class SRA_X : public Instruction {
+public:
+  SRA_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const bool carry = (x & 0x01) != 0x00;
+    const byte_t result = (x & 0x80) | (x >> 1);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class SRA_HL : public Instruction {
+public:
+  SRA_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+    const bool carry = (n & 0x01) != 0x00;
+    const byte_t result = (n & 0x80) | (n >> 1);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class SWAP_X : public Instruction {
+public:
+  SWAP_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const byte_t result = ((x & 0xF0) >> 4) | ((x & 0x0F) << 4);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class SWAP_HL : public Instruction {
+public:
+  SWAP_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+    const byte_t result = ((n & 0xF0) >> 4) | ((n & 0x0F) << 4);
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
+};
+
+template <Register8Bit dst> class SRL_X : public Instruction {
+public:
+  SRL_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const byte_t x = read_reg<dst>();
+    const bool carry = (x & 0x01) != 0x00;
+    const byte_t result = x >> 1;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    write_reg<dst>(result);
+    return {8, 8};
+  }
+};
+
+class SRL_HL : public Instruction {
+public:
+  SRL_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr)
+      : Instruction(reg_file_ptr, bus_ptr) {}
+  std::tuple<std::size_t, std::size_t> step() override {
+    const addr_t addr = read_reg<Register16Bit::REG_HL>();
+    const byte_t n = bus->read_byte(addr);
+    const bool carry = (n & 0x01) != 0x00;
+    const byte_t result = n >> 1;
+
+    // Update flags
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_Z_MASK, result == 0);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_N_MASK);
+    reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_H_MASK);
+    reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
+
+    // Write back
+    bus->write_byte(addr, result);
+    return {16, 8};
+  }
 };
 
 #endif // __BITOPS_H
--- src/cpu/instr/bitops.cpp
+++ src/cpu/instr/bitops.cpp
@@ -23,7 +23,7 @@ std::tuple<std::size_t, std::size_t> CB_PREFIX::step() {
   unique_ptr<Instruction> &ins = lookup.at(op);
 
   // Handle un-implemented opcodes - unlikely because CB doesn't have illegal
-  // opcodes at all, but eh
+  // opcodes at all, but eh. Whatever.
   if (!ins) [[unlikely]] {
     std::ostringstream oss;
     oss << "Unimplemended opcode (0xCB): 0x" << std::uppercase << std::hex
@@ -46,4 +46,76 @@ void LR35902::init_bitops(lookup_table_t &lookup) {
   lookup.at(0xCB) = make_unique<CB_PREFIX>(&reg_file, bus);
 }
 
-void CB_PREFIX::init_cb_prefix(lookup_table_t &lookup) {}
+void CB_PREFIX::init_cb_prefix(lookup_table_t &lookup) {
+  lookup.at(0x00) = make_unique<RLC_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x01) = make_unique<RLC_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x02) = make_unique<RLC_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x03) = make_unique<RLC_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x04) = make_unique<RLC_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x05) = make_unique<RLC_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x06) = make_unique<RLC_HL>(reg_file, bus);
+  lookup.at(0x07) = make_unique<RLC_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x08) = make_unique<RRC_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x09) = make_unique<RRC_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x0A) = make_unique<RRC_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x0B) = make_unique<RRC_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x0C) = make_unique<RRC_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x0D) = make_unique<RRC_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x0E) = make_unique<RRC_HL>(reg_file, bus);
+  lookup.at(0x0F) = make_unique<RRC_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x10) = make_unique<RL_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x11) = make_unique<RL_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x12) = make_unique<RL_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x13) = make_unique<RL_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x14) = make_unique<RL_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x15) = make_unique<RL_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x16) = make_unique<RL_HL>(reg_file, bus);
+  lookup.at(0x17) = make_unique<RL_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x18) = make_unique<RR_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x19) = make_unique<RR_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x1a) = make_unique<RR_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x1b) = make_unique<RR_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x1c) = make_unique<RR_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x1d) = make_unique<RR_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x1e) = make_unique<RR_HL>(reg_file, bus);
+  lookup.at(0x1f) = make_unique<RR_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x20) = make_unique<SLA_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x21) = make_unique<SLA_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x22) = make_unique<SLA_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x23) = make_unique<SLA_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x24) = make_unique<SLA_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x25) = make_unique<SLA_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x26) = make_unique<SLA_HL>(reg_file, bus);
+  lookup.at(0x27) = make_unique<SLA_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x28) = make_unique<SRA_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x29) = make_unique<SRA_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x2a) = make_unique<SRA_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x2b) = make_unique<SRA_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x2c) = make_unique<SRA_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x2d) = make_unique<SRA_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x2e) = make_unique<SRA_HL>(reg_file, bus);
+  lookup.at(0x2f) = make_unique<SRA_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x30) = make_unique<SWAP_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x31) = make_unique<SWAP_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x32) = make_unique<SWAP_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x33) = make_unique<SWAP_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x34) = make_unique<SWAP_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x35) = make_unique<SWAP_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x36) = make_unique<SWAP_HL>(reg_file, bus);
+  lookup.at(0x37) = make_unique<SWAP_X<r8::REG_A>>(reg_file, bus);
+
+  lookup.at(0x38) = make_unique<SRL_X<r8::REG_B>>(reg_file, bus);
+  lookup.at(0x39) = make_unique<SRL_X<r8::REG_C>>(reg_file, bus);
+  lookup.at(0x3a) = make_unique<SRL_X<r8::REG_D>>(reg_file, bus);
+  lookup.at(0x3b) = make_unique<SRL_X<r8::REG_E>>(reg_file, bus);
+  lookup.at(0x3c) = make_unique<SRL_X<r8::REG_H>>(reg_file, bus);
+  lookup.at(0x3d) = make_unique<SRL_X<r8::REG_L>>(reg_file, bus);
+  lookup.at(0x3e) = make_unique<SRL_HL>(reg_file, bus);
+  lookup.at(0x3f) = make_unique<SRL_X<r8::REG_A>>(reg_file, bus);
+}
