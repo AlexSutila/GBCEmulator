6e7c2ec555321c75e68ad4a831a4aacef6b6b687
AlexSutila <alexsutila2020@gmail.com>
2026-01-03 13:36:08 -0700

    Bug fix with DMG coloring

--- src/ppu/ppu.cpp
+++ src/ppu/ppu.cpp
@@ -108,13 +108,13 @@ bool PixelProcessingUnit::should_advance_ly() {
  * pixel FIFO rendering pipeline. This produces an RGB value used directly by
  * our software renderer. Behavior varies between CGB and DMG modes. */
 std::uint32_t PixelProcessingUnit::get_rgb(const pixel &px) const {
-  auto palette_idx = px.color_idx;
+  auto color_idx = px.color_idx;
   /* If we are running in backwards compatability mode, we have to consult the
    * BGP register to translate the monochrome color index. On top of the extra
    * coloring offered with CGB hardware. */
   if (!sys_.cgb_mode)
-    palette_idx = bgp_.get_color_idx(palette_idx);
-  return cram->get_cgb_color(px.color_idx, px.palette_idx);
+    color_idx = bgp_.get_color_idx(color_idx);
+  return cram->get_cgb_color(color_idx, px.palette_idx);
 }
 
 void PixelProcessingUnit::do_oam_scan() {
