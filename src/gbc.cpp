064621cf05ac674ed5b9b850041ef815917a9792
AlexSutila <alexsutila2020@gmail.com>
2026-03-07 16:38:58 -0700

    Get rid of this horrendous macro

--- src/gbc.cpp
+++ src/gbc.cpp
@@ -577,27 +577,26 @@ bool GameBoyColor::savestate_ready() const {
   return cpu && cpu->savestate_ready();
 }
 
-#define SS_WALK(t)                                                             \
-  do {                                                                         \
-    cpu->parse_savestate(t);                                                   \
-    timer->parse_savestate(t);                                                 \
-    ppu->parse_savestate(t);                                                   \
-  } while (0)
-
 std::vector<byte_t> GameBoyColor::savestate_serialize() const {
   Savestate::Writer out{};
-  SS_WALK(out);
+  cpu->parse_savestate(out);
+  timer->parse_savestate(out);
+  ppu->parse_savestate(out);
   return out.get();
 }
 
 void GameBoyColor::savestate_deserialize(const std::span<const byte_t> data) {
   Savestate::Reader in(data);
-  SS_WALK(in);
+  cpu->parse_savestate(in);
+  timer->parse_savestate(in);
+  ppu->parse_savestate(in);
 }
 
 std::size_t GameBoyColor::savestate_size() const {
   Savestate::Sizer sz{};
-  SS_WALK(sz);
+  cpu->parse_savestate(sz);
+  timer->parse_savestate(sz);
+  ppu->parse_savestate(sz);
   return sz.get();
 }
 
