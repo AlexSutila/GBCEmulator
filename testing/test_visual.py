33461cb335ef4f3edd0f027ebb0cf8de0606b602
AlexSutila <alexsutila2020@gmail.com>
2026-04-16 20:02:36 -0700

    Include magen test suite

--- testing/test_visual.py
+++ testing/test_visual.py
@@ -74,6 +74,49 @@ BLARGG_CASES = [
     ),
 ]
 
+MAGEN_CASES = [
+    (
+        "bg_oam_priority",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/bg_oam_priority.gbc",
+        "0c5a8a7405a35f73cdc4268489ad7b6a",
+    ),
+    (
+        "hblank_vram_dma",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/hblank_vram_dma.gbc",
+        "6d4f6a3605a14fab7d6ddd6edb600c97",
+    ),
+    (
+        "key0_lock_after_boot",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/key0_lock_after_boot.gbc",
+        "6d4f6a3605a14fab7d6ddd6edb600c97",
+    ),
+    (
+        "mbc_oob_sram_mbc1",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/mbc_oob_sram_mbc1.gbc",
+        "6d4f6a3605a14fab7d6ddd6edb600c97",
+    ),
+    (
+        "mbc_oob_sram_mbc3",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/mbc_oob_sram_mbc3.gbc",
+        "6d4f6a3605a14fab7d6ddd6edb600c97",
+    ),
+    (
+        "mbc_oob_sram_mbc5",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/mbc_oob_sram_mbc5.gbc",
+        "6d4f6a3605a14fab7d6ddd6edb600c97",
+    ),
+    (
+        "oam_internal_priority",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/oam_internal_priority.gbc",
+        "a08a75fed2c64faae9df5cb3125e8005",
+    ),
+    (
+        "ppu_disabled_state",
+        "https://github.com/alloncm/MagenTests/releases/download/0.5.0/ppu_disabled_state.gbc",
+        "6d4f6a3605a14fab7d6ddd6edb600c97",
+    ),
+]
+
 
 def load_cart_from_url(url: str) -> Cartridge:
     resp = requests.get(url, timeout=30)
@@ -136,7 +179,6 @@ def run_test_set_rendered(
                 transform=ax.transAxes,
                 ha="center",
                 va="top",
-                rotation=-10,
                 fontsize=10,
             )
 
@@ -154,13 +196,23 @@ def run_acid_test_suite():
     )
 
 
-def run_blargg_cpu_tests():
+def run_blargg_tests():
     run_test_set_rendered(
         urls=[i[1] for i in BLARGG_CASES],
         titles=[i[0] for i in BLARGG_CASES],
         rows=2,
         cols=4,
         out_path="assets/blargg_cpu_mem.png",
+    )
+
+
+def run_magen_tests():
+    run_test_set_rendered(
+        urls=[i[1] for i in MAGEN_CASES],
+        titles=[i[0] for i in MAGEN_CASES],
+        rows=2,
+        cols=4,
+        out_path="assets/magen_tests.png",
     )
 
 
@@ -178,6 +230,14 @@ def test_blargg_suite(title: str, url: str, expected_md5: str):
     assert digest == expected_md5, f"{title} failed (got {digest})"
 
 
+@pytest.mark.parametrize("title,url,expected_md5", MAGEN_CASES)
+def test_magen_suite(title: str, url: str, expected_md5: str):
+    img = run_and_get_frame(url)
+    digest = to_digest(img)
+    assert digest == expected_md5, f"{title} failed (got {digest})"
+
+
 if __name__ == "__main__":
     run_acid_test_suite()
-    run_blargg_cpu_tests()
+    run_blargg_tests()
+    run_magen_tests()
