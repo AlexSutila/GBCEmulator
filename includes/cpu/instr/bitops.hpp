8288e00a432b40305630a2dc934d9828b348fe2b
AlexSutila <alexsutila2020@gmail.com>
2026-04-21 16:54:59 -0700

    Reduce code bloat (bitops was the worst offender by far)

--- includes/cpu/instr/bitops.hpp
+++ includes/cpu/instr/bitops.hpp
@@ -178,12 +178,13 @@ private:
   lookup_table_t lookup{};
 };
 
-template <Register8Bit dst> class RLC_X final : public Instruction {
+class RLC_X final : public Instruction {
 public:
-  RLC_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  RLC_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
 
     const bool carry = (x & 0x80) != 0x00;
     const byte_t result = (x << 1) | (carry ? 0x01 : 0x00);
@@ -195,11 +196,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("RLC {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("RLC {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -207,6 +208,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class RLC_HL final : public Instruction {
@@ -258,12 +262,13 @@ private:
   bool c{};
 };
 
-template <Register8Bit dst> class RL_X final : public Instruction {
+class RL_X final : public Instruction {
 public:
-  RL_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  RL_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool old_c = reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK);
     const bool new_c = (x & 0x80) != 0x00;
     const byte_t result = (x << 1) | (old_c ? 0x01 : 0x00);
@@ -275,11 +280,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, new_c);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("RL {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("RL {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -287,6 +292,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class RL_HL final : public Instruction {
@@ -340,12 +348,13 @@ private:
   bool c{};
 };
 
-template <Register8Bit dst> class RRC_X final : public Instruction {
+class RRC_X final : public Instruction {
 public:
-  RRC_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  RRC_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool carry = (x & 0x01) != 0x00;
     const byte_t result = (x >> 1) | (carry ? 0x80 : 0x00);
 
@@ -356,11 +365,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("RRC {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("RRC {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -368,6 +377,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class RRC_HL final : public Instruction {
@@ -421,12 +433,13 @@ private:
   bool c{};
 };
 
-template <Register8Bit dst> class RR_X final : public Instruction {
+class RR_X final : public Instruction {
 public:
-  RR_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  RR_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool old_c = reg_file->reg_af.get_flag(StatusFlagMask::FLAG_C_MASK);
     const bool new_c = (x & 0x01) != 0x00;
     const byte_t result = (x >> 1) | (old_c ? 0x80 : 0x00);
@@ -438,11 +451,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, new_c);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("RR {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("RR {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -450,6 +463,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class RR_HL final : public Instruction {
@@ -503,12 +519,13 @@ private:
   bool c{};
 };
 
-template <Register8Bit dst> class SLA_X final : public Instruction {
+class SLA_X final : public Instruction {
 public:
-  SLA_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  SLA_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool carry = (x & 0x80) != 0x00;
     const byte_t result = x << 1;
 
@@ -519,11 +536,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("SLA {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("SLA {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -531,6 +548,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class SLA_HL final : public Instruction {
@@ -582,12 +602,13 @@ private:
   byte_t n{};
 };
 
-template <Register8Bit dst> class SRA_X final : public Instruction {
+class SRA_X final : public Instruction {
 public:
-  SRA_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  SRA_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool carry = (x & 0x01) != 0x00;
     const byte_t result = (x & 0x80) | (x >> 1);
 
@@ -598,11 +619,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("SLA {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("SLA {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -610,6 +631,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class SRA_HL final : public Instruction {
@@ -661,12 +685,13 @@ private:
   byte_t n{};
 };
 
-template <Register8Bit dst> class SWAP_X final : public Instruction {
+class SWAP_X final : public Instruction {
 public:
-  SWAP_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  SWAP_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const byte_t result = ((x & 0xF0) >> 4) | ((x & 0x0F) << 4);
 
     // Update flags
@@ -676,11 +701,11 @@ public:
     reg_file->reg_af.clr_flag(StatusFlagMask::FLAG_C_MASK);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("SWAP {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("SWAP {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -688,6 +713,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class SWAP_HL final : public Instruction {
@@ -739,12 +767,13 @@ private:
   byte_t n{};
 };
 
-template <Register8Bit dst> class SRL_X final : public Instruction {
+class SRL_X final : public Instruction {
 public:
-  SRL_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  SRL_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool carry = (x & 0x01) != 0x00;
     const byte_t result = x >> 1;
 
@@ -755,11 +784,11 @@ public:
     reg_file->reg_af.put_flag(StatusFlagMask::FLAG_C_MASK, carry);
 
     // Write back
-    write_reg<dst>(result);
+    write_reg(dst, result);
     return 8;
   }
 
-  std::string describe() override { return IroGB::format("SRL {}", to_string<dst>()); }
+  std::string describe() override { return IroGB::format("SRL {}", to_string(dst)); }
 
   InstructionTiming parse() override {
     return {
@@ -767,6 +796,9 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
 };
 
 class SRL_HL final : public Instruction {
@@ -816,12 +848,13 @@ private:
   byte_t n{};
 };
 
-template <byte_t bit, Register8Bit dst> class BIT_N_X final : public Instruction {
+class BIT_N_X final : public Instruction {
 public:
-  BIT_N_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  BIT_N_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst, byte_t bit)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst), bit(bit) {}
 
   std::size_t exec() override {
-    const byte_t x = read_reg<dst>();
+    const byte_t x = read_reg(dst);
     const bool bit_is_zero = ((x >> bit) & 0x01) == 0;
 
     // Update flags
@@ -832,7 +865,7 @@ public:
   }
 
   std::string describe() override {
-    return IroGB::format("BIT {}, {}", static_cast<int>(bit), to_string<dst>());
+    return IroGB::format("BIT {}, {}", static_cast<int>(bit), to_string(dst));
   }
 
   InstructionTiming parse() override {
@@ -841,11 +874,16 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
+  const byte_t bit;
 };
 
-template <byte_t bit> class BIT_N_HL final : public Instruction {
+class BIT_N_HL final : public Instruction {
 public:
-  BIT_N_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  BIT_N_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, byte_t bit)
+      : Instruction(reg_file_ptr, bus_ptr), bit(bit) {}
 
   std::size_t exec() override {
     const addr_t addr = read_reg<Register16Bit::REG_HL>();
@@ -868,21 +906,25 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const byte_t bit;
 };
 
-template <byte_t bit, Register8Bit dst> class RES_N_X final : public Instruction {
+class RES_N_X final : public Instruction {
 public:
-  RES_N_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  RES_N_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst, byte_t bit)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst), bit(bit) {}
 
   std::size_t exec() override {
-    byte_t x = read_reg<dst>();
+    byte_t x = read_reg(dst);
     x &= ~(1 << bit);
-    write_reg<dst>(x);
+    write_reg(dst, x);
     return 8;
   }
 
   std::string describe() override {
-    return IroGB::format("RES {}, {}", static_cast<int>(bit), to_string<dst>());
+    return IroGB::format("RES {}, {}", static_cast<int>(bit), to_string(dst));
   }
 
   InstructionTiming parse() override {
@@ -891,11 +933,16 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
+  const byte_t bit;
 };
 
-template <byte_t bit> class RES_N_HL final : public Instruction {
+class RES_N_HL final : public Instruction {
 public:
-  RES_N_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  RES_N_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, byte_t bit)
+      : Instruction(reg_file_ptr, bus_ptr), bit(bit) {}
 
   std::size_t exec() override {
     addr_t hl = read_reg<Register16Bit::REG_HL>();
@@ -932,21 +979,25 @@ public:
 private:
   InstrStates state{};
   byte_t n{};
+
+private:
+  const byte_t bit;
 };
 
-template <byte_t bit, Register8Bit dst> class SET_N_X final : public Instruction {
+class SET_N_X final : public Instruction {
 public:
-  SET_N_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  SET_N_X(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, Register8Bit dst, byte_t bit)
+      : Instruction(reg_file_ptr, bus_ptr), dst(dst), bit(bit) {}
 
   std::size_t exec() override {
-    byte_t x = read_reg<dst>();
+    byte_t x = read_reg(dst);
     x |= (1 << bit);
-    write_reg<dst>(x);
+    write_reg(dst, x);
     return 8;
   }
 
   std::string describe() override {
-    return IroGB::format("SET {}, {}", static_cast<int>(bit), to_string<dst>());
+    return IroGB::format("SET {}, {}", static_cast<int>(bit), to_string(dst));
   }
 
   InstructionTiming parse() override {
@@ -955,11 +1006,16 @@ public:
         .sync_events = 1,
     };
   }
+
+private:
+  const Register8Bit dst;
+  const byte_t bit;
 };
 
-template <byte_t bit> class SET_N_HL final : public Instruction {
+class SET_N_HL final : public Instruction {
 public:
-  SET_N_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr) : Instruction(reg_file_ptr, bus_ptr) {}
+  SET_N_HL(RegisterFile *reg_file_ptr, AddressBus *bus_ptr, byte_t bit)
+      : Instruction(reg_file_ptr, bus_ptr), bit(bit) {}
   std::size_t exec() override {
     const addr_t hl = read_reg<Register16Bit::REG_HL>();
     switch (state) {
@@ -995,6 +1051,9 @@ public:
 private:
   InstrStates state{};
   byte_t n{};
+
+private:
+  const byte_t bit;
 };
 
 #endif // GBC_BITOPS_HPP
